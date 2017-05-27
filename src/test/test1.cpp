78ce6ae0849516a0a042a70436ea165e243cdf2d
liamplusplus <liamjohn97@hotmail.com>
2017-05-27 16:17:23 +0800

    Pushing final changes

--- src/test/test1.cpp
+++ src/test/test1.cpp
@@ -1,10 +1,27 @@
 #include <ucurses/tests/TestClass.hpp>
+#include <ucurses/ucurses.hpp>
+
+using namespace ucurses;
 
 int main(int argc, char* argv[])
 {
 
-    ucurses::TestClass test;
-    test.Run();
+    //ucurses::TestClass test;
+    
+    ucurses::GUI gui; // Initialize ucurses
+
+    Window::Ptr win = gui.createWindow();
+    /* No parameter overload for full screen */
+    
+    //Window::Ptr win = gui.createWindow(coord2d(50,20),coord2d(5,5)); 
+    /* Specify size and position x,y coordinates respectively */
+
+    mainScreen(win);
+    
+    //test.Run();
+    gui.Run();
+
+
    
 	
 }
